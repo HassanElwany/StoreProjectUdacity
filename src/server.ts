00cc9b775ad258881fba7c4111e8996e0cfd9a16
Hassan Elwany <elwany_hassan@hotmail.com>
2022-04-22 00:08:21 +0300

    integrat db

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,10 @@ import express, { Application, Request, Response } from "express";
 import helmet from "helmet";
 import morgan from "morgan";
 import config from "./config";
-
+import db from "./database";
 console.log(config);
 
-const PORT = config.PORT || 3000;
+const PORT = config.port || 3000;
 const app: Application = express();
 
 app.use(morgan("common"));
@@ -13,6 +13,19 @@ app.use(morgan("common"));
 app.use(express.json());
 
 app.use(helmet());
+
+// db test
+db.connect().then((client) => {
+  return client
+  .query('SELECT NOW()').then((res) =>{
+    client.release();
+    console.log(res.rows);
+  })
+  .catch((err) => {
+    client.release();
+    console.log(err.stack);
+  });
+});
 
 //starting for server
 app.get("/", (req: Request, res: Response) => {
