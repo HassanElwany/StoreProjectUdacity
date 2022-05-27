51497156dc958271017c6badcf894f2bcb90bd6e
HassanElwany <elwany_hassan@hotmail.com>
2022-05-27 22:20:21 +0300

    add jasmine test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World");
+import express, { application, Application, Request, Response } from "express";
+
+const PORT = 3000;
+const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(`Hello from express `);
+});
+
+app.listen(PORT, () => {
+  console.log(`server is running on port: ${PORT}`);
+});
+
+export default app;
